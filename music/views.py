a285de6aadb018325fb81a4f3b30077a36ef3fb3
Mark van der Maas <markvandermaas@gmail.com>
2016-10-11 18:46:41 +0200

    bug fix for NoReverseMatch error on /music/

--- music/views.py
+++ music/views.py
@@ -1,17 +1,10 @@
-#from django.http import Http404
-#from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from .models import Album, Song
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     all_albums = Album.objects.all()
     return render(request, 'music/index.html', {"all_albums": all_albums})
 
 def detail(request, album_id):
-    #try:
-    #    album = Album.objects.get(id=album_id)
-    #except Album.DoesNotExist:
-    #    raise Http404("Woops, this album doesn't exist")
     album = get_object_or_404(Album, id=album_id)
     return render(request, 'music/detail.html', {"album": album})
